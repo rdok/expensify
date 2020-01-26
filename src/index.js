3772d499f64a2643c5980d5216eee271d4a335d3
Rizart Dokollari <r.dokollari@gmail.com>
2020-01-26 19:55:28 +0000

    Header: Bootstrap route links.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.sass'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 
 const DashboardPage = () => (<div>Dashboard Content</div>)
 const AddExpensePage = () => (<div>Add Expense Content</div>)
 const EditExpensePage = () => (<div>Edit Expense Content</div>)
 const HelpPage = () => (<div>Help Content</div>)
-const NotFoundPage = () => (<div>Not Found Content</div>)
+const NotFoundPage = () => (<div>Not Found</div>)
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>
+            Dashboard
+        </NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create</NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+    </header>
+)
 
 const routes = (
     <BrowserRouter>
+        <div>
+            <Header/>
+        </div>
         <Switch>
             <Route path="/" component={DashboardPage} exact={true}/>
             <Route path="/create" component={AddExpensePage}/>

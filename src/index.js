a9ff7c7110e1a41569c75fb083b7f0c694b32809
Rizart Dokollari <r.dokollari@gmail.com>
2020-01-26 19:43:37 +0000

    Setup 404 page.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.sass'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 const DashboardPage = () => (<div>Dashboard Content</div>)
 const AddExpensePage = () => (<div>Add Expense Content</div>)
 const EditExpensePage = () => (<div>Edit Expense Content</div>)
 const HelpPage = () => (<div>Help Content</div>)
+const NotFoundPage = () => (<div>Not Found Content</div>)
 
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={DashboardPage} exact={true}/>
             <Route path="/create" component={AddExpensePage}/>
             <Route path="/edit" component={EditExpensePage}/>
             <Route path="/help" component={HelpPage}/>
-        </div>
+            <Route component={NotFoundPage}/>
+        </Switch>
     </BrowserRouter>
 )
 

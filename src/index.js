378d0b9cb1af5a9f6452b0ba81d0b7f5c03b36ba
Rizart Dokollari <r.dokollari@gmail.com>
2020-01-28 22:08:44 +0000

    Bootstrap redux & setup CI.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.sass'
 
+import {createStore} from 'redux'
 import AppRouter from './routers/AppRouter'
+
+const store = createStore((state = {count: 1}) => {
+    return state
+})
+
+console.log((store.getState()))
+
 
 ReactDOM.render(<AppRouter/>, document.getElementById('root'))

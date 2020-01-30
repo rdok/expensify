3d08dc1450e971c4b9f2939ca5b1345f80316ebd
Rizart Dokollari <r.dokollari@gmail.com>
2020-01-30 12:24:07 +0000

    Redux bootstrap: use actions to change the store's state.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,23 @@ import './styles/styles.sass'
 import {createStore} from 'redux'
 import AppRouter from './routers/AppRouter'
 
-const store = createStore((state = {count: 1}) => {
-    return state
+const store = createStore((state = {count: 0}, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {count: state.count + 1}
+        case 'DECREMENT':
+            return {count: state.count - 1}
+        case 'RESET':
+            return {count: 0}
+        default:
+            return state
+    }
 })
+
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'INCREMENT'})
+store.dispatch({type: 'DECREMENT'})
+store.dispatch({type: 'RESET'})
 
 console.log((store.getState()))
 

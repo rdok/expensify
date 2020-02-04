08cdc51dad4a246020dd49a5926274bf9a34e45e
Rizart Dokollari <r.dokollari@gmail.com>
2020-02-04 12:36:11 +0000

    Introduction to redux store actions.

--- src/index.js
+++ src/index.js
@@ -6,14 +6,28 @@ import './styles/styles.sass'
 import {createStore} from 'redux'
 import AppRouter from './routers/AppRouter'
 
+const incrementCount = ({incrementBy = 1} = {}) => ({
+    type: 'INCREMENT',
+    incrementBy: incrementBy
+})
+
+const decrementCount = ({decrementBy = 1} = {}) => ({
+    type: 'DECREMENT',
+    decrementBy: decrementBy
+})
+
+const setCount = (value) => ({
+    type: 'SET', value
+})
+
+const resetCount = () => ({ type: 'RESET' })
+
 const store = createStore((state = {count: 0}, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = action.incrementBy || 1
-            return {count: state.count + incrementBy}
+            return {count: state.count + action.incrementBy}
         case 'DECREMENT':
-            const decrementBy = action.decrementBy || 1
-            return {count: state.count - decrementBy}
+            return {count: state.count - action.decrementBy}
         case 'SET':
             return {count: action.value}
         case 'RESET':
@@ -27,17 +41,17 @@ const unsubscribe = store.subscribe(() => {
     console.log('subscribe', store.getState())
 })
 
-// store.dispatch({type: 'INCREMENT', incrementBy: 5})
-// store.dispatch({type: 'INCREMENT'})
-// store.dispatch({type: 'INCREMENT'})
+// store.dispatch(incrementCount({incrementBy: 5}))
+// store.dispatch(incrementCount())
 
-// store.dispatch({type: 'DECREMENT'})
-// store.dispatch({type: 'DECREMENT', decrementBy: 3})
+// store.dispatch(decrementCount({decrementBy: 3}))
 
-store.dispatch({type: 'SET', value: 2077})
+// store.dispatch({type: 'SET', value: 2077})
+// store.dispatch(setCount(2077))
 
-unsubscribe()
+store.dispatch(resetCount())
 
-store.dispatch({type: 'RESET'})
+// unsubscribe()
+
 
 ReactDOM.render(<AppRouter/>, document.getElementById('root'))

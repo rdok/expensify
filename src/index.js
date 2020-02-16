ee0e1dc2c48c13d04f81310da82898cb671ca452
Rizart Dokollari <r.dokollari@gmail.com>
2020-02-16 17:16:51 +0000

    Redux data handling: filtering & sorting.

--- src/index.js
+++ src/index.js
@@ -13,29 +13,67 @@ import {
 import {createStore, combineReducers} from 'redux'
 import AppRouter from './routers/AppRouter'
 
+
+const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number'
+            || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number'
+            || expense.createdAt <= endDate;
+
+        const textMatch = expense.description.toLocaleLowerCase()
+            .includes(text.toLowerCase())
+
+        return startDateMatch && endDateMatch && textMatch
+    }).sort((firstExpense, secondExpense) => {
+        switch (sortBy) {
+            case 'date':
+                return firstExpense.createdAt < secondExpense.createdAt
+            case 'amount':
+                return firstExpense.amount < secondExpense.amount
+        }
+    })
+}
+
 const store = createStore(
     combineReducers({expenses: expensesReducer, filters: filtersReducer})
 )
 
 store.subscribe(() => {
-    console.log(store.getState().filters)
+    const state = store.getState()
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+
+    console.log(visibleExpenses)
 })
 
-// const dispatchedExpense = store
-//     .dispatch(addExpense({description: 'Rent', amount: 100}))
+const dispatchedExpense = store
+    .dispatch(addExpense({description: 'RentinG', amount: 100, createdAt: 1000}))
+store.dispatch(addExpense({
+    description: 'Training',
+    amount: 200,
+    createdAt: -1000
+}))
+
+
 // store.dispatch(removeExpense(dispatchedExpense))
 // store.dispatch(editExpense(dispatchedExpense.expense, {description: 'NewDescription'}))
 
 // store.dispatch(setTextFilter('new-text-filter'))
 // store.dispatch(setTextFilter())
 
-// store.dispatch(sortByAmount())
-// store.dispatch(sortByDate())
+store.dispatch(sortByAmount())
+store.dispatch(sortByDate())
 // store.dispatch(sortByAmount())
 
-store.dispatch(setStartDate(115))
-store.dispatch(setEndDate(125))
-store.dispatch(setEndDate())
-store.dispatch(setStartDate())
+// store.dispatch(setTextFilter('notfound'))
+// store.dispatch(setTextFilter('en'))
+// store.dispatch(setTextFilter('ing'))
+
+// store.dispatch(setStartDate(1150))
+// store.dispatch(setStartDate(500))
+// store.dispatch(setStartDate(-1001))
+// store.dispatch(setEndDate(125))
+// store.dispatch(setEndDate())
+// store.dispatch(setStartDate())
 
 ReactDOM.render(<AppRouter/>, document.getElementById('root'))

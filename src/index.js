672f44c7c4f4cad2253e3bce30f80b69b3dab64c
Rizart Dokollari <r.dokollari@gmail.com>
2020-02-18 07:58:15 +0000

    Boostrap higher order component: Info, Auth, & Admin warning.

--- src/index.js
+++ src/index.js
@@ -56,4 +56,42 @@ store.dispatch(setTextFilter('gas'))
 // store.dispatch(setEndDate())
 // store.dispatch(setStartDate())
 
-ReactDOM.render(<AppRouter/>, document.getElementById('root'))
+// ReactDOM.render(<AppRouter/>, document.getElementById('root'))
+
+
+const Info = ({info}) => (
+    <div>
+        <h1>Info</h1>
+        <p>The info is: {info}</p>
+    </div>
+)
+
+const withAdminWarning = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {props.isAdmin && <p>This is private info. Please don't share!</p>}
+            <WrappedComponent {...props}/>
+        </div>
+    )
+}
+
+const AdminInfo = withAdminWarning(Info)
+
+const requireAuth = (WrappedComponent) => {
+    return (props) => (
+        <div>
+            {
+                props.isAuthenticated ? (<p>Redirect to auth page</p>)
+                    : <WrappedComponent {...props}/>
+            }
+
+        </div>
+    )
+}
+
+const AuthInfo = requireAuth(Info)
+
+ReactDOM.render(
+    <AuthInfo isAdmin={true} info="Details Content" isAuthenticated={false}/>,
+    document.getElementById('root')
+)

b1b685d9764381cdc4e63e577f35ca823fa8303b
Rizart Dokollari <r.dokollari@gmail.com>
2020-02-21 20:13:22 +0000

    Filter & remove expenses using reaxt-redux.connect.

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ExpenseList from './ExpenseList'
+import ExpensesListFilter from './ExpensesListFilter'
 
 export const DashboardPage = () => (
     <div>
+        <ExpensesListFilter />
         <ExpenseList/>
     </div>
 )
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {removeExpense} from '../actions/expenses'
 
-const ExpenseListItem = ({description, amount, createdAt}) => (
+const ExpenseListItem = (props) => (
     <div>
-        <h3>{description}</h3>
-        <p>{amount}, {createdAt}</p>
+        <h3>{props.description}</h3>
+        <p>{props.amount}, {props.createdAt}</p>
+        <button onClick={() => {
+            const expense = {id: props.id}
+            props.dispatch(removeExpense({expense}))
+        }}>Remove
+        </button>
     </div>
 )
 
-export default ExpenseListItem
+
+export default connect()(ExpenseListItem)

ca49199f548fd6eb42ce3d8f4026eae679115bfe
Rizart Dokollari <r.dokollari@gmail.com>
2021-11-03 00:00:19 +0000

    Extract unrelated tests

--- tests/selectors/expenses-sort.test.js
+++ tests/selectors/expenses-sort.test.js
@@ -15,25 +15,3 @@ test("Sort expenses by amount", () => {
   const sortedExpenses = selectExpenses(unsortedExpenses, { sortBy: "amount" });
   expect(sortedExpenses[0].id).toEqual(latestExpense.id);
 });
-
-// test(`Filter expenses by startDate`, () => {
-//   let filters = { startDate: expenseBeta.createdAt };
-//   let filtered = selectExpenses(expenses, filters);
-//   expect(filtered.length).toEqual(1);
-//   expect(filtered[0].id).toEqual(expenseBeta.id);
-//
-//   filters = { startDate: expenseAlpha.createdAt };
-//   filtered = selectExpenses(expenses, filters);
-//   expect(filtered.length).toEqual(2);
-// });
-//
-// test(`Filter expenses by endDate`, () => {
-//   let filters = { endDate: expenseAlpha.createdAt };
-//   let filtered = selectExpenses(expenses, filters);
-//   expect(filtered.length).toEqual(1);
-//   expect(filtered[0].id).toEqual(expenseAlpha.id);
-//
-//   filters = { endDate: expenseBeta.createdAt };
-//   filtered = selectExpenses(expenses, filters);
-//   expect(filtered.length).toEqual(2);
-// });

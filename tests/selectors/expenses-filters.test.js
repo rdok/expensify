d945266c1b417e8b1bebf2273d16d4313dd1b9a8
Rizart Dokollari <r.dokollari@gmail.com>
2021-11-03 00:00:49 +0000

    Assert it filters using start & end date

--- tests/selectors/expenses-filters.test.js
+++ tests/selectors/expenses-filters.test.js
@@ -2,34 +2,79 @@ import selectExpenses from "../../src/selectors/expenses";
 import {
   makeExpensesFilterableByDescription,
   makeExpensesFilterableByNote,
+  makeExpensesUnfilteredByEndDate,
+  makeExpensesUnfilteredByStartDate,
 } from "./factories";
 
-describe("When filtering expenses using partial description", () => {
-  it("filters out non matching expenses", () => {
-    const { expenses, partialFilter } = makeExpensesFilterableByDescription();
-    const filteredExpenses = selectExpenses(expenses, { text: partialFilter });
-    expect(filteredExpenses.length).toEqual(1);
+describe("When filtering expenses", () => {
+  describe("With partial description", () => {
+    test("remove all but a single expense", () => {
+      const { expenses, partialFilter } = makeExpensesFilterableByDescription();
+      const text = { text: partialFilter };
+      const filteredExpenses = selectExpenses(expenses, text);
+      expect(filteredExpenses.length).toEqual(1);
+    });
+
+    test("leave the correct expense", () => {
+      const { expenses, partialFilter, expectedExpense } =
+        makeExpensesFilterableByDescription();
+      const text = { text: partialFilter };
+      const filteredExpenses = selectExpenses(expenses, text);
+      expect(filteredExpenses[0]).toHaveProperty("id", expectedExpense.id);
+    });
   });
 
-  it("returns the correct expense", () => {
-    const { expenses, partialFilter, expectedExpense } =
-      makeExpensesFilterableByDescription();
-    const filteredExpenses = selectExpenses(expenses, { text: partialFilter });
-    expect(filteredExpenses[0]).toHaveProperty("id", expectedExpense.id);
-  });
-});
+  describe("With partial note", () => {
+    test("remove all but a single expense", () => {
+      const { expenses, partialFilter } = makeExpensesFilterableByNote();
+      const filteredExpenses = selectExpenses(expenses, {
+        text: partialFilter,
+      });
+      expect(filteredExpenses.length).toEqual(1);
+    });
 
-describe("When filtering expenses using partial note", () => {
-  it("filters out non matching expenses", () => {
-    const { expenses, partialFilter } = makeExpensesFilterableByNote();
-    const filteredExpenses = selectExpenses(expenses, { text: partialFilter });
-    expect(filteredExpenses.length).toEqual(1);
+    test("returns the correct expense", () => {
+      const { expenses, partialFilter, expectedExpense } =
+        makeExpensesFilterableByNote();
+      const filteredExpenses = selectExpenses(expenses, {
+        text: partialFilter,
+      });
+      expect(filteredExpenses[0]).toHaveProperty("id", expectedExpense.id);
+    });
   });
 
-  it("returns the correct expense", () => {
-    const { expenses, partialFilter, expectedExpense } =
-      makeExpensesFilterableByNote();
-    const filteredExpenses = selectExpenses(expenses, { text: partialFilter });
-    expect(filteredExpenses[0]).toHaveProperty("id", expectedExpense.id);
+  describe("With startDate", () => {
+    test("remove all but a single expense", () => {
+      const { unfilteredExpenses, startDate } =
+        makeExpensesUnfilteredByStartDate();
+      const filters = { startDate };
+      const filteredExpenses = selectExpenses(unfilteredExpenses, filters);
+      expect(filteredExpenses.length).toEqual(1);
+    });
+
+    test("returns the correct expense", () => {
+      const { unfilteredExpenses, startDate, expectedExpense } =
+        makeExpensesUnfilteredByStartDate();
+      const filters = { startDate };
+      const filteredExpenses = selectExpenses(unfilteredExpenses, filters);
+      expect(filteredExpenses[0]).toHaveProperty("id", expectedExpense.id);
+    });
+  });
+
+  describe("With endDate", () => {
+    test("remove all but a single expense", () => {
+      const { unfilteredExpenses, endDate } = makeExpensesUnfilteredByEndDate();
+      const filters = { endDate };
+      const filteredExpenses = selectExpenses(unfilteredExpenses, filters);
+      expect(filteredExpenses.length).toEqual(1);
+    });
+
+    test("returns the correct expense", () => {
+      const { unfilteredExpenses, endDate, expectedExpense } =
+        makeExpensesUnfilteredByEndDate();
+      const filters = { endDate };
+      const filteredExpenses = selectExpenses(unfilteredExpenses, filters);
+      expect(filteredExpenses[0]).toHaveProperty("id", expectedExpense.id);
+    });
   });
 });

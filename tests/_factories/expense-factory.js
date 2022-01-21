de08be434c2641a2a9ab9ed2a97472a831fd6c95
Rizart Dokollari <r.dokollari@gmail.com>
2022-01-21 19:03:59 +0000

    Remove references to faker

--- tests/_factories/expense-factory.js
+++ tests/_factories/expense-factory.js
@@ -1,19 +1,17 @@
-import faker from "faker";
-
 export function makeExpenseWithFillable(overwrite = {}) {
-  const description = overwrite.description ?? faker.lorem.sentence();
-  const note = overwrite.note ?? faker.lorem.paragraph();
-  const amount = overwrite.amount ?? faker.datatype.number({ min: 0 });
+  const description = overwrite.description ?? `lorem.sentence()-${new Date()}`;
+  const note = overwrite.note ?? `lorem.paragraph()-${new Date()}`;
+  const amount = overwrite.amount ?? new Date();
 
   return { description, note, amount };
 }
 
 export function makeExpense(overwrite = {}) {
   const fillable = makeExpenseWithFillable(overwrite);
-  const createdAt = overwrite.createdAt ?? faker.date.past();
+  const createdAt = overwrite.createdAt ?? new Date();
   return {
     ...fillable,
-    id: faker.datatype.uuid(),
+    id: `datatype.uuid()-${new Date()}`,
     createdAt,
   };
 }
